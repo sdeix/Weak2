0ca246b6639c73d477cecf9f5bcaa6c377ade0f8
sdeix <test@test.ru>
2024-01-23 21:53:21 +0300

    Реализованно добавление товаров в корзину

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -1,5 +1,6 @@
 <template #default>
 <ul>
+    <button @click="Test2">Тест2</button>
     <li v-for="(prod, index) in prods" :key="prod.id" @click="addProd(index)">id: {{ prod.id }} <br> <b>Название:</b> {{ prod.name }} <br> <b>Описание:</b> {{ prod.description }}</li>
 </ul>
 </template>
@@ -11,13 +12,42 @@ export default {
     computed: {
     prods(){
         return this.$store.getters.getProducts.data
+    },
+    token(){
+        return this.$store.getters.getToken
     }
   },
   methods:{
-    addProd(index){
-        console.log(index)
+    async addProd(index){
+      console.log(this.token)
+        if(this.token){
+          const res = await fetch(`https://jurapro.bhuser.ru/api-shop/cart/${index}`,{
+        method: "POST",
+        headers:{
+          'Content-Type': 'application/json',
+          'Authorization' : `Bearer ${this.token}`
+        }
+      })
+      const data = await res.json()
+        console.log(data)
+        }
+    },    
+    async Test2(){
+      console.log(this.token)
+        if(this.token){
+          const res = await fetch('https://jurapro.bhuser.ru/api-shop/cart',{
+        method: "GET",
+        headers:{
+          'Content-Type': 'application/json',
+          'Authorization' : `Bearer ${this.token}`
+        }
+      })
+      const data = await res.json()
+        console.log(data)
+        }
     }
-  }
+  },
+
 
 }
 
