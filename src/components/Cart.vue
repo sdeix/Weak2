55fbac6c2804fe80231c063fdaf130f5b33dca8f
Сергей Малюгов <gr402_maspe@lab-407-4.tomtit-tomsk.ru>
2024-01-24 13:13:01 +0700

    исправление багов

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -4,12 +4,13 @@
 
 <ul v-if="cart.length">
     <li v-for="(prod, index) in cart" :key="prod.id" ><b>id в корзине:</b> {{ prod.id }} <br> <b>Название:</b> {{ prod.name }} <br> <b>Описание:</b> {{ prod.description }} <br> <b>id товара:</b> {{ prod.product_id }} <br> <b>Цена:</b> {{ prod.price }}
-        <br><b>Колличество:</b> {{ prod.count }}<br>
         <button @click="addProd(prod.product_id)"> Добавить в корзину</button>
         <button @click="removeProd(prod.id)"> Убрать из корзины</button>
     </li>
 </ul>
+
 <h1 v-else>Нет товаров в корзине</h1>
+<button  v-if="cart.length" @click="Order()">Заказать</button>
 </div>
 </template>
 
@@ -19,51 +20,8 @@ export default {
     name: "Cart",
     computed: {
     cart(){
-        const cart1 = this.$store.getters.getCart.data
-        let cart2 = []
-        let cart3 = []
-        for(let u in cart1){
-            cart2.push(cart1[u])
-        }
-        cart3 = cart2.slice(0)
-        let maincart = []
-        for(let i in cart3){
-            if(maincart.length==0){
-                maincart.push({
-                    id:cart3[i].id,
-                    product_id:cart3[i].product_id,
-                    name:cart3[i].name,
-                    description:cart3[i].description,
-                    price:cart3[i].price,
-                    count:1,
-                })           
-            }
-            else{
-                for(let j in maincart){
-                if(cart3[i].product_id==maincart[j].product_id){
-                    maincart[j].count+=1
-                    break
-                }
-                else{
-                    console.log("qqq")
-                    cart3[i].count=0
-                    maincart.push({
-                    id:cart3[i].id,
-                    product_id:cart3[i].product_id,
-                    name:cart3[i].name,
-                    description:cart3[i].description,
-                    price:cart3[i].price,
-                    count:1,
-                }) 
-
-
-                }}
-            }
-
-            
-        }
-        console.log(maincart)
-        return maincart
+        const cart = this.$store.getters.getCart.data
+        return cart
     },
     token(){
         return this.$store.getters.getToken
@@ -94,6 +52,29 @@ export default {
       })
       const data = await res.json()
         this.$store.dispatch('getCart',this.token)
+        }
+    }, 
+    async Order(){
+        console.log(typeof this.token)
+        if(this.token){
+          const res = await fetch(`https://jurapro.bhuser.ru/api-shop/order`,{
+        method: "POST",
+        headers:{
+          'Content-Type': 'application/json',
+          'Authorization' : `Bearer ${this.token}`
+        }
+      })
+      const data = await res.json()
+        this.$store.dispatch('getOrders',this.token)
+        this.$store.dispatch('getCart',this.token)
+        if(res.status==422){
+          this.text="Ошибка, Корзина пуста"
+          setTimeout(() => this.text="", 2000);
+        }
+        else if(res.status==201){
+          this.text="Заказ совершён"
+          setTimeout(() => this.text="", 2000);
+        }
         }
     }, 
    
--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -2,7 +2,7 @@
 <div class="home">
 
     <Cart/>
-    <button @click="Order()">Заказать</button>
+
     <h1 v-if="text">{{ text }}</h1>
 </div>
 </template>
@@ -26,29 +26,7 @@ data() {
   }
 },
 methods:{
-    async Order(){
-        console.log(typeof this.token)
-        if(this.token){
-          const res = await fetch(`https://jurapro.bhuser.ru/api-shop/order`,{
-        method: "POST",
-        headers:{
-          'Content-Type': 'application/json',
-          'Authorization' : `Bearer ${this.token}`
-        }
-      })
-      const data = await res.json()
-        this.$store.dispatch('getOrders',this.token)
-        this.$store.dispatch('getCart',this.token)
-        if(res.status==422){
-          this.text="Ошибка, Корзина пуста"
-          setTimeout(() => this.text="", 2000);
-        }
-        else if(res.status==201){
-          this.text="Заказ совершён"
-          setTimeout(() => this.text="", 2000);
-        }
-        }
-    },  
+ 
 }
 }
 </script>
